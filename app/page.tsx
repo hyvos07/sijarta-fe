2c18a8518cd1f9741de7cfc4ac1cc0f8c3b9e22a
hyvos07 <daniel.liman07@gmail.com>
2024-11-11 16:05:00 +0700

    init: Init Project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-center min-h-screen p-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-4 items-center">
+        <p className="font-semibold text-6xl tracking-wide">SIJARTA</p>
+        <p className="text-lg text-center">Sistem Informasi Jasa Rumah Tangga</p>
+      </main>
+    </div>
+  );
+}
