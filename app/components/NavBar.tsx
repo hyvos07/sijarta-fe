7b558eafb8c1b77c17237a5a81d0cff7afe0deb2
hyvos07 <daniel.liman07@gmail.com>
2024-11-16 21:53:28 +0700

    feat: Navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -8,18 +8,15 @@ export default function NavBar() {
   }
 
   return (
-    <nav className="bg-gray-800 text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="text-white sticky w-full top-0 z-99 bg-black border-b border-stone-800">
+      <div className="mx-auto px-4 sm:px-6 lg:px-16">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold">
+            <Link href="/" className="text-xl font-bold hidden md:block md:tracking-wide">
               Sijarta
             </Link>
           </div>
           <div className="flex space-x-4">
-            <Link href="/login" className="hover:text-gray-300">
-              Login
-            </Link>
             <button 
               onClick={handleLogout}
               className="hover:text-gray-300"
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <div className="items-center justify-center min-h-screen p-16 font-[family-name:var(--font-geist-sans)]">
+      <div className="items-center justify-center min-h-[10000px] p-16 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-4 items-center">
           <p className="font-semibold text-6xl tracking-wide">SIJARTA</p>
           <p className="text-lg text-center">Sistem Informasi Jasa Rumah Tangga</p>
