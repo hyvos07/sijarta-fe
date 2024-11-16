7e540fe00c2b09f9cc69f7ecc8e508d194587731
hyvos07 <daniel.liman07@gmail.com>
2024-11-17 01:13:16 +0700

    feat: Dummy data and data types declaration

---
+++ app/db/services/user.ts
@@ -0,0 +1,19 @@
+import { User, Convert } from '../types/user';
+import userJson from '../data/user.json';
+
+export const userService = {
+    getAllUser: async (): Promise<User[]> => {
+        const jsonString = JSON.stringify(userJson);
+        return Convert.toUser(jsonString);
+    },
+
+    getUserByID: async (id: string): Promise<User | null> => {
+        const user = await userService.getAllUser();
+        return user.find((u) => u.id === id) || null;
+    },
+
+    getUserByPhone: async (noHP: string): Promise<User | null> => {
+        const user = await userService.getAllUser();
+        return user.find((u) => u.noHP === noHP) || null;
+    }
+};
