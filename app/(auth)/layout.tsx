4b5d2db41c7814a584382f3d2f27df7f6c732ce6
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2024-11-18 19:41:02 +0700

    add logo, add error message

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,32 +1,36 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 import { Poppins } from "next/font/google";
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-    title: 'Welcome | SIJARTA',
-    description: 'Sistem Informasi Jasa Rumah Tangga - by AKV',
-    icons: {
-        icon: "favicon.ico",
-    },
-}
+  title: 'Welcome | SIJARTA',
+  description: 'Sistem Informasi Jasa Rumah Tangga - by AKV',
+  icons: {
+    icon: "favicon.ico",
+  },
+};
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={`antialiased ${poppins.className}`}>
-                <main className="flex items-center justify-center min-h-screen">
-                    {children}
-                </main>
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <body className={`antialiased ${poppins.className}`}>
+        <header className="flex items-center justify-center mb-4">
+          <img src="/images/logo.png" alt="SIJARTA Logo" className="h-14 w-14 mr-4" />
+          <h1 className="text-xl font-bold">SIJARTA</h1>
+        </header>
+        <main className="flex items-center justify-center min-h-screen">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }
