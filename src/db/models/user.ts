1d429babbb4ce0f30b9725de8ff4da28aff0db4d
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2024-12-09 20:15:15 +0700

    update profile done

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,8 +1,16 @@
-// path : sijarta-fe/src/db/models/user.ts
+// path: sijarta-fe/src/db/models/user.ts
 
 import { User, Convert } from '../types/user';
 import { BaseModel } from '../model';
 import pool from '../db';
+
+interface UpdateUserData {
+  nama?: string;
+  noHP?: string;
+  jenisKelamin?: 'L' | 'P';
+  tglLahir?: string;
+  alamat?: string;
+}
 
 export class UserModel extends BaseModel<User> {
     constructor() {
@@ -26,6 +34,28 @@ export class UserModel extends BaseModel<User> {
             SET saldo_mypay = saldo_mypay + ${nominal}
             WHERE id = '${id}'
         `);
+        client.release();
+    }
+
+    async updateUser(id: string, data: UpdateUserData): Promise<void> {
+        const client = await pool.connect();
+        const setClauses = [];
+        
+        if (data.nama !== undefined) setClauses.push(`nama = '${data.nama}'`);
+        if (data.noHP !== undefined) setClauses.push(`no_hp = '${data.noHP}'`);
+        if (data.jenisKelamin !== undefined) setClauses.push(`jenis_kelamin = '${data.jenisKelamin}'`);
+        if (data.tglLahir !== undefined) setClauses.push(`tgl_lahir = '${data.tglLahir}'`);
+        if (data.alamat !== undefined) setClauses.push(`alamat = '${data.alamat}'`);
+
+        if (setClauses.length > 0) {
+            const query = `
+                UPDATE ${this.table}
+                SET ${setClauses.join(', ')}
+                WHERE id = '${id}'
+            `;
+            await client.query(query);
+        }
+        
         client.release();
     }
 }
